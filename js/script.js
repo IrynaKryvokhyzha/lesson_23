a260d113b52855e7d2ef886824940018518fd410
KrIryna <95886613+KrIryna@users.noreply.github.com>
2022-11-30 21:53:46 -0800

    aaa

---
+++ js/script.js
@@ -0,0 +1,97 @@
+//цикли for i while//
+
+//Задача 1//
+/*
+let num = 0;
+while (num < 5) {
+	console.log(num);
+	num++;
+}
+*/
+/*
+let num = 0;
+do {
+	console.log(num);
+	num++;
+}
+while (num < 5);
+*/
+/*
+for (let num = 0; num < 5; num++){
+	console.log(num);
+}
+*/
+/*
+let num = 0;
+for (; num < 5; num++){
+	console.log(num);
+}
+*/
+
+//Задача 3//
+/*
+let num = 0;
+while (num < 3) {
+	console.log(`Число: ${num}`);
+	num++;
+}
+*/
+
+//Задача 4//
+/*firstFor: for (let num = 0; num < 2; num++) {
+	for (let size = 0; size < 3; size++){
+		if (size == 1) {
+			break firstFor;
+		} 
+		console.log(size);
+	}
+	
+}
+*/
+//Числа//
+
+//Задача 1//
+/*let numOne = Math.round((1.005 + Number.EPSILON) * 100) / 100;
+console.log(numOne);
+*/
+
+//Задача 2//
+/*let value = "135.58px"
+console.log(parseFloat(value));
+*/
+
+//Задача 3//
+/*let value = 58 + "Freelanser";
+if (58 + "Freelanser" !== NaN) {
+	console.log('Result NaN');
+}
+*/
+
+//Задача 4//
+//console.log(Math.max(10, 58, 39, -150, 0));//
+
+//Задача 5//
+//console.log(Math.floor(58.858));//
+
+//Рядки//
+//Задача 1//
+//Правильно//
+
+//Задача 2//
+/*let text = "Freelanser";
+console.log(text[6]);*/
+
+//Задача 3//
+//ні, воно складеться в рядок//
+
+
+//Задача 4//
+/*let text = "Freelanser";
+console.log(text.toUpperCase());*/
+
+//Задача 5//
+/*let text = "Freelanser";
+console.log(text.slice(4, 7));*/
+
+//Задача 5//
+//true//
